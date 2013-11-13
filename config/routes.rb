f2e7139de25308b23eab939e5f5678026aac6dba
Sara Pak <sarapak0795@gmail.com>
2013-11-12 20:52:46 -0500

    Created home and about pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Pinteresting::Application.routes.draw do
+
+     root :to => 'pages#home'
+      get "about" => "pages#about" #creates about_path
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
